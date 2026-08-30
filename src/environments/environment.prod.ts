986d3e42ae75061874ce821d9f8874f74c3df0c0
Jesus Requena <Elrequena@users.noreply.github.com>
2026-08-30 19:02:00 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,1 @@
-export const environment = { production: true, apiUrl: 'https://requena-labs-api.onrender.com/api' };
+export const environment = { production: true, apiUrl: 'https://simple-store-backend-i7vs.onrender.com/api' };
